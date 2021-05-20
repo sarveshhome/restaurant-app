8c3bb082a2386ffaab0ff25c3a56ed884739835c
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-20 19:49:13 +0100

    add meal items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,49 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sahi Fish',
+    name: 'Fish masala',
     description: 'Finest fish and veggies',
-    price: 22.99,
+    price: 122.99,
   },
   {
     id: 'm2',
-    name: 'Sahni Veg & Non Veg',
-    description: 'A german specialty!',
-    price: 16.5,
+    name: 'Mutton curry',
+    description: 'A indian specialty!',
+    price: 116.5,
   },
   {
     id: 'm3',
-    name: ' Burger',
-    description: 'indian, raw, meaty',
-    price: 12.99,
+    name: 'Indian Burger',
+    description: 'indina, raw, meaty',
+    price: 112.99,
   },
   {
     id: 'm4',
-    name: 'Mutton Masal',
+    name: 'Sahni Veg',
     description: 'Healthy...and green...',
-    price: 18.99,
+    price: 118.99,
   },
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
