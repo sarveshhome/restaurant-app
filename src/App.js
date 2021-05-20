d4fb075162f9767cae1c23b990c81ad78bed2f42
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-20 19:15:42 +0100

    add header with menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-          Learn React
-       
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
