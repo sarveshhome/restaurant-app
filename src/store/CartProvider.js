d5a3d380176997ab49a66c54ca2936eadb687574
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-22 08:15:59 +0100

    cart Reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -30,7 +30,7 @@ const CartProvider = (props) => {
     dispatchCartAction({type: 'REMOVE', id: id});
   };
 
-  const cartContext = {
+  const cartContext = {    
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
