264b2af45870ab696885ff79376370d7f6118d7d
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-22 13:38:25 +0100

    Complex Reducer Logic and use Effect hook

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -29,10 +29,12 @@ const DUMMY_MEALS = [
   },
 ];
 
+
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
+      id={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
